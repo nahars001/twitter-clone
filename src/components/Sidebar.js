1ecfbe70f49fac694dd087dedb9a32e99e3c9600
Nahar Singh <naharsingh151299@gmail.com>
2023-01-13 14:43:36 +0530

    side bar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,22 @@
+import { Twitter } from "@mui/icons-material";
+import React from "react";
+import './Sidebar.css';
+
+const  Sidebar  = () => {
+
+    return (
+        <div className="'sidebar"> 
+        {/* twiter icon */}
+        <Twitter/>
+        {/* side bar options */}
+        {/* tweet button */}
+        
+        </div>
+
+
+    );
+}
+
+
+
+export default Sidebar;
