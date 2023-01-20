09cb87b033c4042141041ed77aabec3d4997fe8f
Nahar Singh <naharsingh151299@gmail.com>
2023-01-20 12:29:51 +0530

    env

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBABNxOJ2BFios3kRsKSrnw_zCGuU9Q6WE",
-    authDomain: "twitter-clone-nahar.firebaseapp.com",
-    databaseURL: "https://twitter-clone-nahar-default-rtdb.asia-southeast1.firebasedatabase.app",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
     projectId: "twitter-clone-nahar",
     storageBucket: "twitter-clone-nahar.appspot.com",
-    messagingSenderId: "784918657949",
-    appId: "1:784918657949:web:efdbf70efa778092fd0d74"
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 
